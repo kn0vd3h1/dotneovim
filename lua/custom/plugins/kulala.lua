1e59c7a987c638064ff9ba74bc5d99504f4d1c4e
capthiron <24925821+capthiron@users.noreply.github.com>
2025-09-25 16:07:36 +0200

    feat: add kulalal.lua as REST client

---
+++ lua/custom/plugins/kulala.lua
@@ -0,0 +1,10 @@
+return {
+  'mistweaverco/kulala.nvim',
+  ft = { 'http', 'rest' },
+  opts = {
+    -- your configuration comes here
+    global_keymaps = true,
+    global_keymaps_prefix = '<leader>r',
+    kulala_keymaps_prefix = '',
+  },
+}
