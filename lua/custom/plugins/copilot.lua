a4da04b8657d4a750039af603f6034495e529b7b
capthiron <24925821+capthiron@users.noreply.github.com>
2025-09-25 16:05:37 +0200

    refactor: replace github/copilot with lua wrapper

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,8 +1,30 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
   config = function()
-    vim.g.copilot_filetypes = {
-      ['gitcommit'] = true,
+    require('copilot').setup {
+      suggestion = {
+        keymap = {
+          accept = '<Tab>',
+          next = '<M-j>',
+          prev = '<M-k>',
+        },
+      },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        gitcommit = true,
+      },
     }
+
+    -- Function to toggle Copilot suggestions
+    function CopilotEnable()
+      require('copilot.suggestion').toggle_auto_trigger()
+      print 'Copilot auto-trigger enabled ✅'
+    end
+
+    -- Create user commands to call the functions
+    vim.api.nvim_create_user_command('CopilotEnable', CopilotEnable, { desc = 'Enable Copilot suggestions' })
   end,
 }
