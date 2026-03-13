7c2e2a988f1818ed6ac4169fb5061e770da43e2d
capthiron <24925821+capthiron@users.noreply.github.com>
2026-03-13 16:39:50 +0100

    chore: add gt keybinding to go to type definition

--- init.lua
+++ init.lua
@@ -578,6 +578,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Go to the type definition of the symbol under the cursor.
+          map('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
